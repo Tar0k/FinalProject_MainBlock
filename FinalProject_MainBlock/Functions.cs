2754453f35241bc5fb55e21d63440d09e6683b68
Tar0k <73.tarok@gmail.com>
2023-06-20 23:40:50 +0400

    1 commit

---
+++ FinalProject_MainBlock/Functions.cs
@@ -0,0 +1,176 @@
+﻿namespace FinalProject_MainBlock;
+
+public static class Functions
+{
+    /// <summary>
+    /// Request correct user input of int variable.
+    /// </summary>
+    /// <param name="variableDescription">variable description in input line</param>
+    /// <param name="minLimit">minimum available input</param>
+    /// <param name="maxLimit">maximum available input</param>
+    /// <returns>A 32-bit integer, which inserted by user in console.</returns>
+    public static int  GetUserInt(
+        string variableDescription = "целочисленное значение",
+        int? minLimit = null,
+        int? maxLimit = null)
+    {
+        while (true)
+        {
+            Console.Write($"Введите {variableDescription}: ");
+            var userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("!!!НИЧЕГО НЕ ВВЕДЕНО!!!");
+                continue;
+            }
+
+            if (!int.TryParse(userInput, out var userInt))
+            {
+                Console.WriteLine("!!!НЕ КОРРЕКТНЫЙ ВВОД!!!");
+                continue;
+            }
+
+            if (minLimit.HasValue & userInt < minLimit)
+            {
+                Console.WriteLine($"!!!ВВЕДЕНОЕ ЗНАЧЕНИЕ МЕНЬШЕ ДОПУСТИМОГО ({minLimit})!!!");
+                continue;
+            }
+            
+            if (maxLimit.HasValue & userInt > maxLimit)
+            {
+                Console.WriteLine($"!!!ВВЕДЕНОЕ ЗНАЧЕНИЕ ВЫШЕ ДОПУСТИМОГО ({maxLimit})!!!");
+                continue;
+            }
+            return userInt;
+        }
+    }
+    
+    /// <summary>
+    /// Create 2D array with random integer values
+    /// </summary>
+    /// <param name="rows">number of rows</param>
+    /// <param name="columns">number of columns</param>
+    /// <param name="minElement">minimum random generated element</param>
+    /// <param name="maxElement">maximum random generated element</param>
+    /// <returns>A Int[rows,columns] array filled with random values between minElement and maxElement.</returns>
+    public static int[,] Generate2DIntArray(
+        int rows,
+        int columns,
+        int minElement = 1,
+        int maxElement = 10
+        )
+    {
+        var array = new int[rows, columns];
+        var random = new Random();
+        for (var row = 0; row < array.GetLength(0); row++)
+        {
+            for (var column = 0; column < array.GetLength(1); column++)
+            {
+                array[row, column] = random.Next(minElement, maxElement);
+            }
+        }
+    
+        return array;
+    }
+    
+    /// <summary>
+    /// Create 2D array with random double values
+    /// </summary>
+    /// <param name="rows">number of rows</param>
+    /// <param name="columns">number of columns</param>
+    /// <param name="decimalPlaces">number of symbols past dot</param>
+    /// <param name="minElement">minimum random generated element</param>
+    /// <param name="maxElement">maximum random generated element</param>
+    /// <returns>A Double[rows,columns] array filled with random values between minElement and maxElement.</returns>
+    public static double[,] Generate2DDoubleArray(
+        int rows,
+        int columns,
+        int decimalPlaces = 0,
+        int minElement = 1, 
+        int maxElement = 10
+        )
+    {
+        var array = new double[rows, columns];
+        var random = new Random();
+        for (var row = 0; row < array.GetLength(0); row++)
+        {
+            for (var column = 0; column < array.GetLength(1); column++)
+            {
+                array[row, column] = Math.Round(random.NextDouble() * (maxElement - minElement) + minElement, decimalPlaces);
+            }
+        }
+        return array;
+    }
+    
+    /// <summary>
+    /// Print out 2D array
+    /// </summary>
+    /// <param name="array">array, to be printed out</param>
+    public static void Print2DArray(int[,] array) 
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    /// <summary>
+    /// Print out 2D array
+    /// </summary>
+    /// <param name="array">array, to be printed out</param>
+    public static void Print2DArray(double[,] array) 
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    /// <summary>
+    /// Print out 2D array with aligned columns by adding filler in front.
+    /// </summary>
+    /// <param name="array">array, to be printed out</param>
+    /// <param name="filler">symbol, which to be added to string in front</param>
+    public static void AdvancedPrint2DArray(int[,] array, string filler = " ")
+    {
+        var maxLengthElement = LookingForLongestElement(array);
+        
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(IntToStringWithLength(array[i,j], maxLengthElement, filler) + " ");
+            }
+            Console.WriteLine();
+        }   
+        
+        
+        int LookingForLongestElement(int[,] arr)
+        {
+            var length = 0;
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j].ToString().Length > length) length = array[i, j].ToString().Length;
+                }
+            }
+
+            return length;
+        }
+
+        string IntToStringWithLength(int value, int length, string fillerText)
+        {
+            var text = value.ToString();
+            while (text.Length < length) text = fillerText + text;
+            return text;
+        }
+    }
+}
---
+++ FinalProject_MainBlock/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+//     Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+using static FinalProject_MainBlock.Functions;
+
+var examples = new List<string[]>()
+{
+    new [] {"Hello", "2", "world", ":-)" },
+    new [] {"1234", "1567", "-2", "computer science"},
+    new [] {"Russia", "Denmark", "Kazan"}
+};
+
+Console.WriteLine("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,\n" +
+                  " длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,\n" +
+                  " либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,\n" +
+                  " лучше обойтись исключительно массивами.");
+Console.WriteLine("---------------------------------------------------------------------------------------------------");
+Console.WriteLine("1. Выбрать ввод из \"примеров\"");
+Console.WriteLine("2. Ввести массив в ручную");
+Console.WriteLine("---------------------------------------------------------------------------------------------------");
+
+var userSelect = GetUserInt(variableDescription: "номер опции", minLimit:1, maxLimit:2);
+
+string[] testArray = 
+
+switch (userSelect)
+{
+    case 1:
+        foreach (var example in examples.Select((item, index) => (item, index)))
+        {
+            Console.WriteLine($"{example.index + 1}: Пример {FormattedStringFromArray(example.item)}");
+        }
+        var chosenExample = GetUserInt(variableDescription: "номер \"примера\"", minLimit:1, maxLimit: examples.Count);
+        testArray = examples[chosenExample - 1];
+        break;
+    case 2:
+        var testArraySize = GetUserInt(variableDescription: "размер массива", minLimit: 1);
+        testArray = new string[testArraySize];
+        for (var i=0; i < testArray.Length; i++)
+        {
+            Console.Write($"Введите {i + 1} элемент: ");
+            testArray[i] = Console.ReadLine() ?? "";
+        }
+        Console.WriteLine($"Введен массив {FormattedStringFromArray(testArray)}");
+        break;
+}
+Console.WriteLine(FormattedStringFromArray(testArray));
+
+
+
+
+string FormattedStringFromArray(IEnumerable<string> array)
+{
+    return "[" + string.Join(", ", array.Select(str => "“" + str + "“")) + "]";
+}
